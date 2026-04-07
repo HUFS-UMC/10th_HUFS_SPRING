7df994985491f9ec4937bec359558c1b8caa51a5
minjun <minjun0306@hufs.ac.kr>
2026-04-07 12:38:35 +0900

    feat: 4주차 프로젝트 초기 세팅 및 도메인 아키텍처 구조 생성 및 swagger 설정

---
+++ 야르_김민준/umc10th/src/main/java/com/example/umc10th/domain/member/entitiy/mapping/MemberFood.java
@@ -0,0 +1,4 @@
+package com.example.umc10th.domain.member.entitiy.mapping;
+
+public class MemberFood {
+}
---
+++ 야르_김민준/umc10th/src/main/java/com/example/umc10th/domain/mission/entitiy/mapping/MemberMission.java
@@ -0,0 +1,4 @@
+package com.example.umc10th.domain.mission.entitiy.mapping;
+
+public class MemberMission {
+}
