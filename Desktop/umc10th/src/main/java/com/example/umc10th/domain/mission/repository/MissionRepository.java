d395f540465057de31ec25a10e50d09b060429a3
Seoyeon-park0103 <seoyeon-park@naver.com>
2026-05-14 00:32:31 +0900

    feat : 7주차 미션

--- Desktop/umc10th/src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
+++ Desktop/umc10th/src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
@@ -1,4 +1,17 @@
 package com.example.umc10th.domain.mission.repository;
 
-public interface MissionRepository {
+import com.example.umc10th.domain.mission.entity.Mission;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MissionRepository extends JpaRepository<Mission,Long> {
+    List<Mission> findAllByStore_Id(Long storeId);
+    // 커서 없이 조회
+    Slice<Mission> findMissionsByStore_IdOrderByIdDesc(Long storeId, Pageable pageable);
+
+    // 커서 기반 조회
+    Slice<Mission> findMissionByStore_IdAndIdLessThanOrderByIdDesc(Long storeId, Long idCursor, Pageable pageable);
 }
