d798c9869adc9320a3d6b8ac82ece6eef0b436fd
Park Wonho <olivyou2@gmail.com>
2026-05-25 12:34:32 +0900

    feat: Passkey implemented

--- 원호_박원호/sample_project/src/main/java/com/wonho/sample_project/global/config/SecurityConfig.java
+++ 원호_박원호/sample_project/src/main/java/com/wonho/sample_project/global/config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.wonho.sample_project.global.config;
 
+import org.hibernate.sql.exec.spi.JdbcOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.webauthn.management.JdbcPublicKeyCredentialUserEntityRepository;
+import org.springframework.security.web.webauthn.management.JdbcUserCredentialRepository;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    private final String[] ALLOWED_ORIGINS = {
+            "*"
+    };
+
     private final String[] allowUris = {
             // Swagger 허용
             "/swagger-ui/**",
@@ -38,7 +46,11 @@ public class SecurityConfig {
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
                         .permitAll()
-                );
+                )
+                .webAuthn(webAuth ->
+                        webAuth.rpId("localhost")
+                                .allowedOrigins(ALLOWED_ORIGINS)
+                                .disableDefaultRegistrationPage(true));
 
         return http.build();
     }
@@ -46,5 +58,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JdbcPublicKeyCredentialUserEntityRepository jdbcPublicKeyCredentialUserEntityRepository(JdbcOperations jdbc) {
+        return new JdbcPublicKeyCredentialUserEntityRepository(jdbc);
+    }
+
+    @Bean
+    public JdbcUserCredentialRepository jdbcUserCredentialRepository(JdbcOperations jdbc) {
+        return new JdbcUserCredentialRepository(jdbc);
     }
 }
